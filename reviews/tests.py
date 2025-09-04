34e606c164cf0920d48557b78acb0846c5d2df9e
Kalanza <kalanzavictor@gmail.com>
2025-09-04 22:45:15 +0300

    Add unit tests for Review API: implement tests for listing, authenticated creation, and unauthenticated creation of reviews

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import Review
 
-# Create your tests here.
+class ReviewAPITestCase(APITestCase):
+	def setUp(self):
+		self.user = User.objects.create_user(username='testuser', password='testpass')
+		self.review = Review.objects.create(
+			movie_title='Inception',
+			review_content='Great movie!',
+			rating=5,
+			user=self.user
+		)
+
+	def test_review_list(self):
+		url = reverse('review-list')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+	def test_review_create_auth(self):
+		self.client.login(username='testuser', password='testpass')
+		url = reverse('review-list')
+		data = {
+			'movie_title': 'Interstellar',
+			'review_content': 'Amazing!',
+			'rating': 5
+		}
+		response = self.client.post(url, data)
+		self.assertEqual(response.status_code, 201)
+
+	def test_review_create_unauth(self):
+		url = reverse('review-list')
+		data = {
+			'movie_title': 'Dunkirk',
+			'review_content': 'Good!',
+			'rating': 4
+		}
+		response = self.client.post(url, data)
+		self.assertEqual(response.status_code, 401)
