37808e6a518db3c79748d9aff5d03877f01a7a24
Kalanza <kalanzavictor@gmail.com>
2025-09-07 21:17:30 +0300

    Refactor settings.py to use environment variable for DEBUG and update ALLOWED_HOSTS

--- moviereviewapi/settings.py
+++ moviereviewapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!vm4=%#ukw=(p#ttxlrm-+(x1sx2n5izw4*84^0qyqp-2=ur$="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kalanzaa.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework_simplejwt"
+    "rest_framework_simplejwt",
     "reviews",  
 ]
 
@@ -159,5 +160,5 @@ REST_FRAMEWORK = {
     },
 }
 
-STATIC_ROOT = BASE_DIR / "static"
+
 
