36d1464f3fd603639a56d80998d5bc99ffa8e8e0
Kalanza <kalanzavictor@gmail.com>
2025-11-21 21:16:37 +0300

    Enhance admin interface with all models and better display options

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review, UserProfile, ReviewLike, ReviewComment
 
-admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('movie_title', 'rating', 'user', 'created_date')
+    list_filter = ('rating', 'created_date', 'user')
+    search_fields = ('movie_title', 'review_content', 'user__username')
+    ordering = ('-created_date',)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+    search_fields = ('user__username', 'bio')
+
+@admin.register(ReviewLike)
+class ReviewLikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'review', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'review__movie_title')
+
+@admin.register(ReviewComment)
+class ReviewCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'review', 'content', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'review__movie_title', 'content')
+    ordering = ('-created_at',)
