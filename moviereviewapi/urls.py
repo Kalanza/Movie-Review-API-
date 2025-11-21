3980ccb7d65ef87460e29ed7130eee9057e5f08b
Kalanza <kalanzavictor@gmail.com>
2025-11-21 22:10:50 +0300

    Fix schema view URLs to use path instead of re_path

--- moviereviewapi/urls.py
+++ moviereviewapi/urls.py
@@ -45,15 +45,10 @@ def test_view(request):
 
 urlpatterns = [
     # API Documentation (put these first to avoid conflicts)
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', 
-            schema_view.without_ui(cache_timeout=0), 
-            name='schema-json'),
-    re_path(r'^swagger/$', 
-            schema_view.with_ui('swagger', cache_timeout=0), 
-            name='schema-swagger-ui'),
-    re_path(r'^redoc/$', 
-            schema_view.with_ui('redoc', cache_timeout=0), 
-            name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
     # Test endpoint
     path('test/', test_view, name='test'),
