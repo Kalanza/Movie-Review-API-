67f2183ee74c38e1b417311f9f012d67d2bd42b1
Kalanza <kalanzavictor@gmail.com>
2025-11-21 23:27:18 +0300

    Remove invalid swagger_ui_settings from get_schema_view

--- moviereviewapi/urls.py
+++ moviereviewapi/urls.py
@@ -41,10 +41,6 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
    authentication_classes=(),
-   swagger_ui_settings={
-       'url': '',
-       'persistAuthorization': True,
-   },
 )
 
 def test_view(request):
